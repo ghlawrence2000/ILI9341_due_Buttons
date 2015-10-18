d0b2fa886d7489b8153a495a1fb4747eceb3e3de
Graham Lawrence <ghlawrence2000@yahoo.co.uk>
2015-10-18 21:42:40 +0100

    First commit
    First attempt at a port of UTFT_Buttons to ILI9341_due.
    
    Main effort by GHLawrence with assistance and debugging contributions
    from David_Prentice & TFTLCDCyg. Thanks.

---
+++ ILI9341_due_Buttons.cpp
@@ -0,0 +1,276 @@
+// Ported to ILI9341_due by GHLawrence2000 (perfected by David Prentice)
+//
+// ghlawrence2000@yahoo.co.uk
+//
+// https://github.com/ghlawrence2000
+//
+/*
+  ILI9341_due_Buttons.cpp - Add-on Library for UTFT: Buttons
+  Copyright (C)2015 Rinky-Dink Electronics, Henning Karlsen. All right reserved
+
+  This library adds simple but easy to use buttons to extend the use
+  of the UTFT and UTouch libraries.
+
+  You can find the latest version of the library at
+  http://www.RinkyDinkElectronics.com/
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the CC BY-NC-SA 3.0 license.
+  Please see the included documents for further information.
+
+  Commercial use of this library requires you to buy a license that
+  will allow commercial use. This includes using the library,
+  modified or not, as a tool to sell products.
+
+  The license applies to all part of the library including the
+  examples and tools supplied with the library.
+*/
+
+#include "ILI9341_due_Buttons.h"    //.kbv defines UTouch_SPI
+#include <ILI9341_due.h>
+#include <UTouch.h>
+
+ILI9341_due_Buttons::ILI9341_due_Buttons(ILI9341_due *ptrILI9341, UTouch *ptrUTouch)
+{
+  _ILI9341 = ptrILI9341;
+  _UTouch = ptrUTouch;
+  deleteAllButtons();
+  _color_text       = ILI9341_WHITE;
+  _color_text_inactive  = ILI9341_GRAY;
+  _color_background   = ILI9341_BLUE;
+  _color_border     = ILI9341_WHITE;
+  _color_hilite     = ILI9341_RED;
+  _font_text        = NULL;
+  _font_symbol      = NULL;
+}
+
+int ILI9341_due_Buttons::addButton(uint16_t x, uint16_t y, uint16_t width, uint16_t height, char *label, uint16_t flags)
+{
+  int btcnt = 0;
+
+  while (((buttons[btcnt].flags & BUTTON_UNUSED) == 0) and (btcnt < MAX_BUTTONS))
+    btcnt++;
+
+  if (btcnt == MAX_BUTTONS)
+    return -1;
+  else
+  {
+    buttons[btcnt].pos_x  = x;
+    buttons[btcnt].pos_y  = y;
+    buttons[btcnt].width  = width;
+    buttons[btcnt].height = height;
+    buttons[btcnt].flags  = flags;
+    buttons[btcnt].label  = label;
+    buttons[btcnt].data   = NULL;
+    return btcnt;
+  }
+}
+
+int ILI9341_due_Buttons::addButton(uint16_t x, uint16_t y, uint16_t width, uint16_t height, bitmapdatatype data, uint16_t flags)
+{
+  int btcnt = 0;
+
+  while (((buttons[btcnt].flags & BUTTON_UNUSED) == 0) and (btcnt < MAX_BUTTONS))
+    btcnt++;
+
+  if (btcnt == MAX_BUTTONS)
+    return -1;
+  else
+  {
+    buttons[btcnt].pos_x  = x;
+    buttons[btcnt].pos_y  = y;
+    buttons[btcnt].width  = width;
+    buttons[btcnt].height = height;
+    buttons[btcnt].flags  = flags | BUTTON_BITMAP;
+    buttons[btcnt].label  = NULL;
+    buttons[btcnt].data   = data;
+    return btcnt;
+  }
+}
+
+
+void ILI9341_due_Buttons::drawButtons()
+{
+  for (int i = 0; i < MAX_BUTTONS; i++)
+  {
+    if ((buttons[i].flags & BUTTON_UNUSED) == 0)
+      drawButton(i);
+  }
+}
+
+void ILI9341_due_Buttons::drawButton(int buttonID)  //.kbv use wid, ht for Rect
+{
+  int   text_x, text_y;
+  const unsigned char *_font_current = _ILI9341->_font;
+  word  _current_color = _ILI9341->_fontColor;
+  word  _current_back  = _ILI9341->_fontBgColor;
+
+  if (buttons[buttonID].flags & BUTTON_BITMAP)
+  {
+    _ILI9341->drawImage(buttons[buttonID].data, buttons[buttonID].pos_x, buttons[buttonID].pos_y, buttons[buttonID].width, buttons[buttonID].height);
+    if (!(buttons[buttonID].flags & BUTTON_NO_BORDER))
+    {
+      if ((buttons[buttonID].flags & BUTTON_DISABLED))
+        _ILI9341->drawRect(buttons[buttonID].pos_x, buttons[buttonID].pos_y, buttons[buttonID].width, buttons[buttonID].height, _color_text_inactive);
+      else
+        _ILI9341->drawRect(buttons[buttonID].pos_x, buttons[buttonID].pos_y, buttons[buttonID].width, buttons[buttonID].height, _color_border);
+    }
+  }
+  else
+  {
+    //_UTFT->setColor(_color_background);
+    _ILI9341->fillRoundRect(buttons[buttonID].pos_x, buttons[buttonID].pos_y, buttons[buttonID].width, buttons[buttonID].height, 5, _color_background);
+    //_UTFT->setColor(_color_border);
+    _ILI9341->drawRoundRect(buttons[buttonID].pos_x, buttons[buttonID].pos_y, buttons[buttonID].width, buttons[buttonID].height, 5, _color_border);
+    if (buttons[buttonID].flags & BUTTON_DISABLED)
+      _ILI9341->setTextColor(_color_text_inactive, _color_background);
+    else
+      _ILI9341->setTextColor(_color_text, _color_background);
+    if (buttons[buttonID].flags & BUTTON_SYMBOL)
+    {
+      _ILI9341->setFont(_font_symbol);
+      text_x = (buttons[buttonID].width / 2) - (_ILI9341->charWidth('a', _font_symbol) / 2) + buttons[buttonID].pos_x;
+      text_y = (buttons[buttonID].height / 2) - (_ILI9341->fontHeight() / 2) + buttons[buttonID].pos_y;
+    }
+    else
+    {
+      _ILI9341->setFont(_font_text);
+      text_x = (buttons[buttonID].width / 2) - (_ILI9341->stringWidth(buttons[buttonID].label) / 2) + buttons[buttonID].pos_x; //.kbv
+      text_y = (buttons[buttonID].height / 2) - (_ILI9341->fontHeight() / 2) + buttons[buttonID].pos_y;
+    }
+    //_UTFT->setBackColor(_color_background);
+    _ILI9341->printAt(buttons[buttonID].label, text_x, text_y);
+    if ((buttons[buttonID].flags & BUTTON_SYMBOL) and (buttons[buttonID].flags & BUTTON_SYMBOL_REP_3X))
+    {
+      _ILI9341->printAt(buttons[buttonID].label, text_x - _ILI9341->charWidth('a', _font_symbol), text_y);
+      _ILI9341->printAt(buttons[buttonID].label, text_x + _ILI9341->charWidth('a', _font_symbol), text_y);
+    }
+  }
+  _ILI9341->setFont(_font_current);       //???????????????
+  _ILI9341->setTextColor(_current_color, _current_back);     //???????????????
+  //_ILI9341->setBackColor(_current_back);  //???????????????
+}
+
+void ILI9341_due_Buttons::enableButton(int buttonID, boolean redraw)
+{
+  if (!(buttons[buttonID].flags & BUTTON_UNUSED))
+  {
+    buttons[buttonID].flags = buttons[buttonID].flags & ~BUTTON_DISABLED;
+    if (redraw)
+      drawButton(buttonID);
+  }
+}
+
+void ILI9341_due_Buttons::disableButton(int buttonID, boolean redraw)
+{
+  if (!(buttons[buttonID].flags & BUTTON_UNUSED))
+  {
+    buttons[buttonID].flags = buttons[buttonID].flags | BUTTON_DISABLED;
+    if (redraw)
+      drawButton(buttonID);
+  }
+}
+
+void ILI9341_due_Buttons::relabelButton(int buttonID, char *label, boolean redraw)
+{
+  if (!(buttons[buttonID].flags & BUTTON_UNUSED))
+  {
+    buttons[buttonID].label = label;
+    if (redraw)
+      drawButton(buttonID);
+  }
+}
+
+boolean ILI9341_due_Buttons::buttonEnabled(int buttonID)
+{
+  return !(buttons[buttonID].flags & BUTTON_DISABLED);
+}
+
+void ILI9341_due_Buttons::deleteButton(int buttonID)
+{
+  if (!(buttons[buttonID].flags & BUTTON_UNUSED))
+    buttons[buttonID].flags = BUTTON_UNUSED;
+}
+
+void ILI9341_due_Buttons::deleteAllButtons()
+{
+  for (int i = 0; i < MAX_BUTTONS; i++)
+  {
+    buttons[i].pos_x = 0;
+    buttons[i].pos_y = 0;
+    buttons[i].width = 0;
+    buttons[i].height = 0;
+    buttons[i].flags = BUTTON_UNUSED;
+    buttons[i].label = "";
+  }
+}
+
+int ILI9341_due_Buttons::checkButtons()
+{
+  if (_UTouch->dataAvailable() == true)
+  {
+    _UTouch->read();
+    int   result = -1;
+    int   touch_x = _UTouch->getX();
+    int   touch_y = _UTouch->getY();
+    //word  _current_color = _UTFT->getColor(); ???????????????????????
+
+    for (int i = 0; i < MAX_BUTTONS; i++)
+    {
+      if (((buttons[i].flags & BUTTON_UNUSED) == 0) and ((buttons[i].flags & BUTTON_DISABLED) == 0) and (result == -1))
+      {
+        if ((touch_x >= buttons[i].pos_x) and (touch_x <= (buttons[i].pos_x + buttons[i].width)) and (touch_y >= buttons[i].pos_y) and (touch_y <= (buttons[i].pos_y + buttons[i].height)))
+          result = i;
+      }
+    }
+    if (result != -1)
+    {
+      if (!(buttons[result].flags & BUTTON_NO_BORDER))
+      {
+        //_UTFT->setColor(_color_hilite);
+        if (buttons[result].flags & BUTTON_BITMAP)
+          _ILI9341->drawRect(buttons[result].pos_x, buttons[result].pos_y, buttons[result].width, buttons[result].height, _color_hilite);
+        else
+          _ILI9341->drawRoundRect(buttons[result].pos_x, buttons[result].pos_y, buttons[result].width, buttons[result].height, 5, _color_hilite);
+      }
+    }
+
+    while (_UTouch->dataAvailable() == true) {};
+
+    if (result != -1)
+    {
+      if (!(buttons[result].flags & BUTTON_NO_BORDER))
+      {
+        //_UTFT->setColor(_color_border);
+        if (buttons[result].flags & BUTTON_BITMAP)
+          _ILI9341->drawRect(buttons[result].pos_x, buttons[result].pos_y, buttons[result].width, buttons[result].height, _color_border);
+        else
+          _ILI9341->drawRoundRect(buttons[result].pos_x, buttons[result].pos_y, buttons[result].width, buttons[result].height, 5, _color_border);
+      }
+    }
+
+    //_UTFT->setColor(_current_color); ???????????
+    return result;
+  }
+  else
+    return -1;
+}
+
+void ILI9341_due_Buttons::setTextFont(gTextFont font)
+{
+  _font_text = font;
+}
+
+void ILI9341_due_Buttons::setSymbolFont(gTextFont font)
+{
+  _font_symbol = font;
+}
+
+void ILI9341_due_Buttons::setButtonColors(word atxt, word iatxt, word brd, word brdhi, word back)
+{
+  _color_text       = atxt;
+  _color_text_inactive  = iatxt;
+  _color_background   = back;
+  _color_border     = brd;
+  _color_hilite     = brdhi;
+}
---
+++ examples/ILI9341_due_Buttons_Demo_320x240/ILI9341_due_Buttons_Demo_320x240.ino
@@ -0,0 +1,166 @@
+// Ported to ILI9341_due by GHLawrence2000
+//
+// ghlawrence2000@yahoo.co.uk
+//
+// https://github.com/ghlawrence2000
+//
+// UTFT_Buttons_Demo_320x240
+// Copyright (C)2015 Rinky-Dink Electronics, Henning Karlsen. All right reserved
+// web: http://www.RinkyDinkElectronics.com/
+//
+// A small demo to demonstrate the use of some of the
+// functions of the UTFT_Buttons add-on library.
+//
+// This demo was made for modules with a screen resolution
+// of 320x240 pixels, but should work on larger screens as
+// well.
+//
+// This program requires both the UTFT and UTouch libraries
+// in addition to the UTFT_Buttons add-on library.
+//
+#include <SPI.h>
+#include <ILI9341_due_config.h>
+#include <ILI9341_due.h>
+#include "SmallFont.h"
+#include "BigFont.h"
+#include "Dingbats1_XL.h"
+
+#include <UTouch.h>
+#include <ILI9341_due_Buttons.h>
+
+// Declare which fonts we will be using
+//extern uint8_t SmallFont[];
+//extern uint8_t BigFont[];
+//extern uint8_t Dingbats1_XL[];
+
+// For the Adafruit shield, these are the default.
+#define TFT_RST 8
+#define TFT_DC 9
+#define TFT_CS 10
+
+// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
+ILI9341_due tft = ILI9341_due(TFT_CS, TFT_DC, TFT_RST);
+
+// Set up UTouch...
+// Set the pins to the correct ones for your development board
+// -----------------------------------------------------------
+// Standard Arduino 2009/Uno/Leonardo shield   : 15,10,14,9,8
+// Standard Arduino Mega/Due shield            : 6,5,4,3,2
+// CTE TFT LCD/SD Shield for Arduino Due       : 6,5,4,3,2
+// Standard chipKit Uno32/uC32                 : 20,21,22,23,24
+// Standard chipKit Max32                      : 62,63,64,65,66
+// AquaLEDSource All in One Super Screw Shield : 62,63,64,65,66
+// CC3200 LaunchPad (pins used in the examples): 31,13,19,28,17
+//
+UTouch        myTouch(6, 5, 4, 3, 2);
+
+// Finally we set up UTFT_Buttons :)
+ILI9341_due_Buttons  myButtons(&tft, &myTouch);
+
+void setup()
+{
+  //myGLCD.InitLCD();
+  //myGLCD.clrScr();
+  Serial.begin(115200);
+  // Initial setup
+  tft.begin();
+  tft.setRotation(iliRotation270);  // landscape
+  tft.fillScreen(ILI9341_BLACK);
+
+  //myGLCD.setFont(SmallFont);
+  tft.setFont(SmallFont);
+  //int fontx = tft.charWidth('a', SmallFont);
+  //int fonty = tft.fontHeight();
+  //Serial.println(fontx);
+  //Serial.println(fonty);
+
+  myTouch.InitTouch();
+  myTouch.setPrecision(PREC_MEDIUM);
+
+  myButtons.setTextFont(BigFont);
+  myButtons.setSymbolFont(Dingbats1_XL);
+}
+
+void loop()
+{
+  int but1, but2, but3, but4, butX, butY, pressed_button;
+  boolean default_colors = true;
+
+  but1 = myButtons.addButton( 10,  20, 300,  30, "Button 1");
+  but2 = myButtons.addButton( 10,  60, 300,  30, "Button 2");
+  but3 = myButtons.addButton( 10, 100, 300,  30, "Button 3");
+  but4 = myButtons.addButton( 10, 140, 300,  30, "Button 4", BUTTON_DISABLED);
+  butX = myButtons.addButton(279, 199,  40,  40, "a", BUTTON_SYMBOL);
+  butY = myButtons.addButton(  0, 199, 100,  40, "I", BUTTON_SYMBOL | BUTTON_SYMBOL_REP_3X);
+  myButtons.drawButtons();
+
+  tft.printAt("You pressed:", 110, 205);
+  //myGLCD.setColor(VGA_BLACK);
+  //myGLCD.setBackColor(VGA_WHITE);
+  tft.setTextColor(ILI9341_BLACK, ILI9341_WHITE);
+  tft.printAt("None    ", 110, 220);
+
+  while (1)
+  {
+    if (myTouch.dataAvailable() == true)
+    {
+      pressed_button = myButtons.checkButtons();
+
+      if (pressed_button == butX)
+      {
+        if (myButtons.buttonEnabled(but4))
+          myButtons.disableButton(but4, true);
+        else
+          myButtons.enableButton(but4, true);
+      }
+      else if (pressed_button == butY)
+      {
+        if (default_colors)
+        {
+          myButtons.setButtonColors(ILI9341_YELLOW, ILI9341_RED, ILI9341_YELLOW, ILI9341_BLUE, ILI9341_GRAY);
+          myButtons.relabelButton(butY, "_");
+          myButtons.drawButtons();
+          default_colors = false;
+        }
+        else
+        {
+          myButtons.setButtonColors(ILI9341_WHITE, ILI9341_GRAY, ILI9341_WHITE, ILI9341_RED, ILI9341_BLUE);
+          myButtons.relabelButton(butY, "I");
+          myButtons.drawButtons();
+          default_colors = true;
+        }
+      }
+      if (pressed_button == but1)
+      {
+        //myGLCD.print("Button 1", 110, 220);
+        tft.printAt("Button 1", 110, 220);
+        //Serial.println("Button 1");
+      }
+
+      if (pressed_button == but2)
+      {
+        //myGLCD.print("Button 2", 110, 220);
+        tft.printAt("Button 2", 110, 220);
+        //Serial.println("Button 2");
+      }
+      if (pressed_button == but3)
+      {
+        //myGLCD.print("Button 3", 110, 220);
+        tft.printAt("Button 3", 110, 220);
+        //Serial.println("Button 3");
+      }
+      if (pressed_button == but4)
+      {
+        //myGLCD.print("Button 4", 110, 220);
+        tft.printAt("Button 4", 110, 220);
+        //Serial.println("Button 4");
+      }
+      if (pressed_button == -1)
+      {
+        //myGLCD.print("None    ", 110, 220);
+        tft.printAt("None    ", 110, 220);
+      }
+    }
+  }
+}
+
